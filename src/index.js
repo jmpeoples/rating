1c68ca67d3548b2cfc326c54d9e1aaa3129d2b61
jmpeoples <jmpeoples.88@gmail.com>
2020-09-03 09:05:59 -0500

    Initial Commit

--- src/index.js
+++ src/index.js
@@ -3,11 +3,21 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { BrowserRouter } from 'react-router-dom';
+import { Security } from '@okta/okta-react';
+
+const oktaConfig = {
+  issuer: `${process.env.REACT_APP_OKTA_ORG_URL}/oauth2/default`,
+  redirect_uri: `${window.location.origin}/implicit/callback`,
+  client_id: process.env.REACT_APP_OKTA_CLIENT_ID,
+};
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Security {...oktaConfig}>
+      <App />
+    </Security>
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
